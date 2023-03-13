10d352223e8fb504ab26a6b317e82b462972046a
Allen Guan <me@nella17.tw>
2023-03-13 18:16:22 +0800

    [pow] multiprocessing.pool

--- pow/pow.py
+++ pow/pow.py
@@ -5,79 +5,45 @@ import base64
 import hashlib
 import time
 from pwn import *
+from multiprocessing.pool import Pool
 
-def solve_pow(r, lower = 0, upper = 1000000000, debug = True, event = None):
-    prefix = r.recvline().decode().split("'")[1]
-    if debug: print(time.time(), "solving pow ...")
-    solved = b''
-    for i in range(lower, upper):
+def task(args):
+    prefix, i = args
+    while True:
         h = hashlib.sha1((prefix + str(i)).encode()).hexdigest()
         if h[:6] == '000000':
             solved = str(i).encode()
-            if 1 or debug: print("solved =", solved)
-            if debug: print(time.time(), "done.")
-            r.sendlineafter(b'string S: ', base64.b64encode(solved))
-            return True
-        if event and event.is_set():
-            break
-    return False
+            return solved
+        i += 1
 
-from threading import Event
-from concurrent import futures
+def solve_pow(r, lower = 0, upper = 1000000000, count = 8):
+    prefix = r.recvline().decode().split("'")[1]
+    info(f"{time.time():.5f} solving pow ...")
+    solved = b''
+    with Pool(processes=count) as pool:
+        it = pool.imap_unordered(task, [(prefix, lower + x * upper // count) for x in range(count)])
+        solved = next(it)
+        success(f"solved = {solved}")
+        info(f"{time.time():.5f} done.")
+        r.sendlineafter(b'string S: ', base64.b64encode(solved))
+        return True
 
-def create_connect(event, host, port, lower, upper, *args, **kwargs):
-    with context.local(log_level='ERROR'):
-        io = remote(host, port, *args, **kwargs)
-        if solve_pow(io, lower, upper, False, event):
-            event.set()
-            return io
-        io.close()
-
-def wrap_func(event, func = create_connect, *args, **kwargs):
-    while not event.is_set():
-        result = func(event, *args, **kwargs)
-        if result:
-            event.set()
-            return result
-
-def connect_pow(host, port, count = 10, *args, **kwargs):
-    event = Event()
-    with futures.ThreadPoolExecutor(max_workers = count) as executor:
-        while True:
-            conn = [
-                executor.submit(
-                    create_connect, event,
-                    host=host, port=port,
-                    lower = 0,
-                    upper = 1000000000,
-                    *args, **kwargs
-                )
-            ]
-            for _ in range(count):
-                time.sleep(1 / count)
-                conn.append(
-                    executor.submit(
-                        wrap_func, event, create_connect,
-                        host=host, port=port,
-                        lower = 50000,
-                        upper = 60000,
-                        *args, **kwargs
-                    )
-                )
-            for future in futures.as_completed(conn):
-                result = future.result()
-                if result:
-                    return result
+def create_connect(*args, **kwargs):
+    io = remote(*args, **kwargs)
+    solve_pow(io)
+    return io
 
 if __name__ == '__main__':
-    r = connect_pow('up23.zoolab.org', 10330)
-    print(r.readall().decode())
-    exit(0)
+    if 1:
+        r = create_connect('up23.zoolab.org', 10330)
+    else:
+        #r = remote('localhost', 10330);
+        r = remote('up23.zoolab.org', 10330)
+        solve_pow(r)
 
-    #r = remote('localhost', 10330);
-    r = remote('up23.zoolab.org', 10330)
-    solve_pow(r)
-    r.interactive()
+    #  r.interactive()
+    r.shutdown()
+    print(r.recv().decode())
     r.close()
 
 # vim: set tabstop=4 expandtab shiftwidth=4 softtabstop=4 number cindent fileencoding=utf-8 :
