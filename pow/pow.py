2113aadf9bb5de67bcd49662e00bfdf08a42ecf5
Allen Guan <me@nella17.tw>
2023-03-13 13:16:03 +0800

    [pow] concurrent

--- pow/pow.py
+++ pow/pow.py
@@ -6,21 +6,74 @@ import hashlib
 import time
 from pwn import *
 
-def solve_pow(r):
+def solve_pow(r, lower = 0, upper = 1000000000, debug = True, event = None):
     prefix = r.recvline().decode().split("'")[1]
-    print(time.time(), "solving pow ...")
+    if debug: print(time.time(), "solving pow ...")
     solved = b''
-    for i in range(1000000000):
+    for i in range(lower, upper):
         h = hashlib.sha1((prefix + str(i)).encode()).hexdigest()
         if h[:6] == '000000':
             solved = str(i).encode()
-            print("solved =", solved)
-            break;
-    print(time.time(), "done.")
+            if 1 or debug: print("solved =", solved)
+            if debug: print(time.time(), "done.")
+            r.sendlineafter(b'string S: ', base64.b64encode(solved))
+            return True
+        if event and event.is_set():
+            break
+    return False
 
-    r.sendlineafter(b'string S: ', base64.b64encode(solved))
+from threading import Event
+from concurrent import futures
+
+def create_connect(event, host, port, lower, upper, *args, **kwargs):
+    with context.local(log_level='ERROR'):
+        io = remote(host, port, *args, **kwargs)
+        if solve_pow(io, lower, upper, False, event):
+            event.set()
+            return io
+        io.close()
+
+def wrap_func(event, func = create_connect, *args, **kwargs):
+    while not event.is_set():
+        result = func(event, *args, **kwargs)
+        if result:
+            event.set()
+            return result
+
+def connect_pow(host, port, count = 10, *args, **kwargs):
+    event = Event()
+    with futures.ThreadPoolExecutor(max_workers = count) as executor:
+        while True:
+            conn = [
+                executor.submit(
+                    create_connect, event,
+                    host=host, port=port,
+                    lower = 0,
+                    upper = 1000000000,
+                    *args, **kwargs
+                )
+            ]
+            for _ in range(count):
+                time.sleep(1 / count)
+                conn.append(
+                    executor.submit(
+                        wrap_func, event, create_connect,
+                        host=host, port=port,
+                        lower = 50000,
+                        upper = 60000,
+                        *args, **kwargs
+                    )
+                )
+            for future in futures.as_completed(conn):
+                result = future.result()
+                if result:
+                    return result
 
 if __name__ == '__main__':
+    r = connect_pow('up23.zoolab.org', 10330)
+    print(r.readall().decode())
+    exit(0)
+
     #r = remote('localhost', 10330);
     r = remote('up23.zoolab.org', 10330)
     solve_pow(r)
