b6ca2c17eb1143de16e6a2f9b26943613262faec
Allen Guan <me@nella17.tw>
2023-05-08 15:09:58 +0800

    [Lab/7] done

---
+++ Lab/7/shm.c
@@ -0,0 +1,26 @@
+#include <sys/shm.h>
+
+#define	ARRAY_SIZE	0x40000
+#define	MALLOC_SIZE	0x100000
+#define	SHM_SIZE	0x100000
+#define	SHM_MODE	0600	/* user read/write */
+
+char	array[ARRAY_SIZE];	/* uninitialized data = bss */
+
+int
+main(void) {
+	int		shmid;
+	char	*ptr, *shmptr;
+
+    int key = 0x1337;
+	if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) < 0)
+		perror("shmget error");
+	if ((shmptr = shmat(shmid, 0, 0)) == (void *)-1)
+		perror("shmat error");
+	printf("shared memory attached from %lx to %lx\n",
+	  (unsigned long)shmptr, (unsigned long)shmptr+SHM_SIZE);
+
+    strcpy(shmptr, "m30w");
+
+	exit(0);
+}
