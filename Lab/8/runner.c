1e93486facf53c4b63f79787f53bb1863c8b6656
Allen Guan <me@nella17.tw>
2023-05-22 15:57:35 +0800

    [Lab/8] no fork

--- Lab/8/runner.c
+++ Lab/8/runner.c
@@ -11,8 +11,6 @@
 #define SIZE 10
 #define errquit(m) { perror(m); exit(-1); }
 
-const char asmfork[] = "j9X\x0f\x05\x90\x90\xCC";
-
 void dump_regs(struct user_regs_struct regs) {
 	fprintf(stderr, "[%s] rax=%-16llx rbx=%-16llx rcx=%-16llx rdx=%-16llx\n",
 		"", regs.rax, regs.rbx, regs.rcx, regs.rdx);
@@ -69,62 +67,31 @@ int main(int argc, char const *argv[]) {
     assert(WIFSTOPPED(status));
 
     if (ptrace(PTRACE_GETREGS, child, 0, &regs) < 0) errquit("ptrace(GETREGS)");
-    void* main = (void*)regs.rip;
-    fprintf(stderr, "[*] main  = %p\n", main);
-
-    void* fork = main - strlen(asmfork);
-    assert(strlen(asmfork) == sizeof(long));
-    long dat = *((long*)asmfork);
-    if (ptrace(PTRACE_POKETEXT, child, fork, dat) < 0) errquit("ptrace(POKETEXT)");
-    fprintf(stderr, "[*] fork  = %p\n", fork);
-    fprintf(stderr, "[*] asm   = 0x%lx\n", dat);
-
-    // dump_regs(regs);
+    struct user_regs_struct saved_regs = regs;
+    fprintf(stderr, "[*] main  = %p\n", (void*)saved_regs.rip);
 
     for (long long n = 0; n < (1<<SIZE); n++) {
-        if (ptrace(PTRACE_GETREGS, child, 0, &regs) < 0) errquit("ptrace(GETREGS)");
-        regs.rip = (long)fork;
-        if (ptrace(PTRACE_SETREGS, child, 0, &regs) < 0) errquit("ptrace(POKEUSER)");
+        if (ptrace(PTRACE_SETREGS, child, 0, &saved_regs) < 0) errquit("ptrace(POKEUSER)");
 
-        pid_t worker = -1;
-
-        for (int i = 0; i < 2; i++) {
-            PTRACE_WAIT(child, status);
-            assert(WIFSTOPPED(status));
-        }
-
-        if (ptrace(PTRACE_GETREGS, child, 0, &regs) < 0) errquit("ptrace(GETREGS)");
-        // dump_regs(regs);
-        worker = regs.rax;
-
-        if (ptrace(PTRACE_GETREGS, child, 0, &regs) < 0) errquit("ptrace(GETREGS)");
-        // dump_regs(regs);
-
-        // fprintf(stderr, "[*] worker= %d\n", worker);
-
-        PTRACE_WAIT(worker, status);
+        PTRACE_WAIT(child, status);
         assert(WIFSTOPPED(status));
-
-        // if (ptrace(PTRACE_GETREGS, worker, 0, &regs) < 0) errquit("ptrace(GETREGS)");
-        // dump_regs(regs);
 
         char buf[8];
         for (int i = 0; i < 8; i++)
             buf[i % 8] = '0' + ((n >> i) & 1);
-        if (ptrace(PTRACE_POKETEXT, worker, magic, *(long*)buf) < 0) errquit("ptrace(POKETEXT)");
+        if (ptrace(PTRACE_POKETEXT, child, magic, *(long*)buf) < 0) errquit("ptrace(POKETEXT)");
         memset(buf, 0, 8);
         for (int i = 8; i < SIZE; i++)
             buf[i % 8] = '0' + ((n >> i) & 1);
-        if (ptrace(PTRACE_POKETEXT, worker, magic+8, *(long*)buf) < 0) errquit("ptrace(POKETEXT)");
+        if (ptrace(PTRACE_POKETEXT, child, magic+8, *(long*)buf) < 0) errquit("ptrace(POKETEXT)");
 
         for (;;) {
-            PTRACE_WAIT(worker, status);
+            PTRACE_WAIT(child, status);
             if (!WIFSTOPPED(status)) break;
-            if (ptrace(PTRACE_GETREGS, worker, 0, &regs) < 0) errquit("ptrace(GETREGS)");
+            if (ptrace(PTRACE_GETREGS, child, 0, &regs) < 0) errquit("ptrace(GETREGS)");
             // dump_regs(regs);
             if (regs.rax == 0xffffffff) break;
         }
-        kill(worker, SIGKILL);
         if (!WIFSTOPPED(status)) break;
     }
 
